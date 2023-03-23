63ca81c6faaf9e1272077a85b93374a0ee236c7f
jangjaeko <jangjaeko45@gmail.com>
2023-03-23 20:25:52 +0900

    add express

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { firestore } = require("firebase-admin");
 var admin = require("firebase-admin");
-
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
@@ -23,4 +25,19 @@ const testData = {
   name: "testData",
 };
 
-db.collection("testData").doc(testData.id.toString()).set(testData);
+// db.collection("testData").doc(testData.id.toString()).set(testData);
+
+db.collection("testData")
+  .doc("1")
+  .delete()
+  .then((res) => {
+    console.log("success");
+  });
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`server is listening at localhost:${process.env.PORT}`);
+});
